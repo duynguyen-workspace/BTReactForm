022ad1df90e2b08a8398aa4423c3529ee8c295b3
duynguyen-workspace <duynguyen.workspace@gmail.com>
2023-11-01 01:05:21 +0700

    add search feature

--- src/components/ProductTable.jsx
+++ src/components/ProductTable.jsx
@@ -1,28 +1,41 @@
 import { useSelector } from "react-redux"
 import ProductRow from "./ProductRow"
+import { useState } from "react"
 
 const ProductTable = () => {
     const { productList } = useSelector((state) => state.btForm)
+    const [searchValue, setSearchValue ] = useState("")
     console.log('Product List:', productList)
     
     return (
-        <table className="table table-hover mt-5">
-            <thead>
-                <tr className="table-dark">
-                    <th scope="col" className="text-warning">ID</th>
-                    <th scope="col" className="text-warning">Image</th>
-                    <th scope="col" className="text-warning">Name</th>
-                    <th scope="col" className="text-warning">Price</th>
-                    <th scope="col" className="text-warning">Description</th>
-                    <th scope="col" colSpan={2} className="text-warning">Type</th>
-                </tr>
-            </thead>
-            <tbody>
-                {productList.map((product) => {
-                    return (<ProductRow key={product.id} item={product}/>)
-                })}
-            </tbody>
-        </table>
+        <>
+            <div className="mt-5 text-center p-relative">
+
+                <input className="w-50 p-2 border border-secondary rounded" type="text" placeholder="search a product..." onChange={(ev) => {
+                    setSearchValue(ev.target.value.toLowerCase())
+                }}/>
+            </div>
+            <table className="table table-hover mt-2">
+                <thead>
+                    <tr className="table-dark">
+                        <th scope="col" className="text-warning">ID</th>
+                        <th scope="col" className="text-warning">Image</th>
+                        <th scope="col" className="text-warning">Name</th>
+                        <th scope="col" className="text-warning">Price</th>
+                        <th scope="col" className="text-warning">Description</th>
+                        <th scope="col" colSpan={2} className="text-warning">Type</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {productList.map((product) => {
+                        if (product.name.toLowerCase().indexOf(searchValue) !== -1) {
+                            return (<ProductRow key={product.id} item={product}/>)
+                        }
+                        
+                    })}
+                </tbody>
+            </table>
+        </>
     )
 }
 
